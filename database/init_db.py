86d2e2673ae20d30d34d6b2761238dd1ef6f67ba
Jewel <116877680+Jewelmj@users.noreply.github.com>
2025-11-16 23:34:27 +0530

    feat(db): initialize SQLite database setup

---
+++ database/init_db.py
@@ -0,0 +1,9 @@
+from database.db import Base, engine
+from database.models import AssetORM, PlaceORM
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+    print("Database initialized.")
+
+if __name__ == "__main__":
+    init_db()
---
+++ database/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, String, Integer
+from database.db import Base
+
+class AssetORM(Base):
+    __tablename__ = "assets"
+
+    id = Column(String, primary_key=True)
+    name = Column(String)
+    category = Column(String)
+
+
+class PlaceORM(Base):
+    __tablename__ = "places"
+
+    id = Column(String, primary_key=True)
+    name = Column(String)
+    location = Column(String)
